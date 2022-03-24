9f7ee9f20015bb1f779e0208bf7ab7503fd9ac87
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-03-23 20:39:34 -0600

    Pipe async

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -50,5 +51,41 @@ cambiarCliente():void{
 borrarCliente(): void{
 this.clientes = this.clientes.pop() != 'undefined' ? this.clientes : this.clientesVacios;
 }
+
+//keyvalue Pipe
+persona = {
+  nombre: 'Vero',
+  edad: 37,
+  direccion: 'CDMX'
+}
+
+//Json Pipe
+heroes = [
+  {
+    nombre: 'Superman',
+    vuela: true
+  },
+  {
+    nombre: 'Robin',
+    vuela: false
+  },
+  {
+    nombre: 'Mujer Maravilla',
+    vuela: true
+  }
+];
+
+
+//Async Pipe
+
+miObservable = interval(1000);
+
+valorPromesa = new Promise( ( resolve, reject  ) => {
+
+  setTimeout(() => {
+    resolve('Tenemos data de promesa')
+  }, 3500);
+
+} );
 
 }
